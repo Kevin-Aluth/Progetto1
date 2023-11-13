732f64e81c7007461440ab488f2e8155c46a88ea
Kevin <kevinaluthdurageanton@gmail.com>
2023-11-13 16:46:26 +0100

    Update Mixer

--- Account.java
+++ Account.java
@@ -40,17 +40,4 @@ class Account {
         }
         
     }
-
-    public String getUsername() {
-        return username;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- Login.java
+++ Login.java
@@ -5,32 +5,33 @@ public class Login {
     Login instance = null; 
 
     private Login(){}
-    public Login LoginClass()
-    {
-        if(instance == null)
-        {
+    public Login LoginClass(){
+        if(instance == null){
             instance = this; 
         }
         return instance;
     }
 
-    public Account checkLogin(String username, String password)
-    {
+    public Account checkLogin(String username, String password){
         Account activeAccount = null;  
         
-        for(int i = 0; i < accounts.size(); i++)
-        {
-            if(accounts.get(i).getUsername() == username)
-            {
+        for(int i = 0; i < accounts.size(); i++){
+            if(accounts.get(i).getUsername() == username){
                 activeAccount = accounts.get(i); 
                 break; 
             }
         }
         if(activeAccount == null) return null; 
-        if(activeAccount.getPassword() != password)
-        {
+        if(activeAccount.getPassword() != password){
             return null; 
         }
         return activeAccount; 
+    }
+
+    public ArrayList<Account> getAccounts(){
+        return accounts; 
+    }
+    public void setAccount(Account account){
+        accounts.add(account); 
     }
 }
