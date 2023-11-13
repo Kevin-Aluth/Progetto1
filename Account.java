1561b1e57e0bfc7f1beacbec19b9e108dcde44ec
Kevin <kevinaluthdurageanton@gmail.com>
2023-11-13 15:50:47 +0100

    First commit.

---
+++ Account.java
@@ -0,0 +1,4 @@
+public class Account {
+    String username; 
+    String password; 
+}
---
+++ Login.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Login {
+    ArrayList<Account> accounts = new ArrayList<>(); 
+
+    //add singleton
+}
