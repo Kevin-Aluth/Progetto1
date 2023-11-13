2d5000c54ab68c99923df7a107a2097cc04f3cd7
Kevin <kevinaluthdurageanton@gmail.com>
2023-11-13 16:15:50 +0100

    Funzione login

--- Account.java
+++ Account.java
@@ -1,4 +1,17 @@
 public class Account {
     String username; 
     String password; 
+
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- Login.java
+++ Login.java
@@ -1,7 +1,27 @@
 import java.util.ArrayList;
 
 public class Login {
-    ArrayList<Account> accounts = new ArrayList<>(); 
+    ArrayList<Account> accounts = new ArrayList<>();
+
+    Account checkLogin(String username, String password)
+    {
+        Account activeAccount = null;  
+        
+        for(int i = 0; i < accounts.size(); i++)
+        {
+            if(accounts.get(i).getUsername() == username)
+            {
+                activeAccount = accounts.get(i); 
+                break; 
+            }
+        }
+        if(activeAccount == null) return null; 
+        if(activeAccount.getPassword() != password)
+        {
+            return null; 
+        }
+        return activeAccount; 
+    }
 
     //add singleton
 }
